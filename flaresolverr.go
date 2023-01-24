935c0390bd2d767e985b3e4514229c655284f068
Fahim Bagar <fahim.bagar@gmail.com>
2023-01-25 00:15:28 +0700

    flaresolverr-request: Add GET and POST request

---
+++ flaresolverr.go
@@ -0,0 +1,295 @@
+package flaresolverr
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	defaultBaseURL = "http://localhost:8191/v1"
+
+	contentApplicationJSON = "application/json"
+)
+
+// Config holds parameters for calling Flaresolverr NewClient.
+type Config struct {
+	// BaseURL is for Flaresolverr URL. Default: http://localhost:8191/v1.
+	BaseURL string
+	// Global Timeout to solve the challenge in milliseconds. Default: No timeout.
+	Timeout int
+}
+
+// Client is a simple wrapper around the general Flaresolverr and represents
+// a client to talk with Flaresolverr API.
+type Client struct {
+	baseURL string
+	timeout int
+}
+
+// NewClient is the constructor for Flaresolverr API Client.
+func NewClient(c Config) (*Client, error) {
+	var baseURL string
+	switch c.BaseURL {
+	case "":
+		baseURL = defaultBaseURL
+	default:
+		if _, err := url.Parse(c.BaseURL); err != nil {
+			return nil, err
+		}
+
+		baseURL = c.BaseURL
+	}
+
+	return &Client{
+		baseURL: baseURL,
+		timeout: c.Timeout,
+	}, nil
+}
+
+type command string
+
+const (
+	get  command = "request.get"
+	post command = "request.post"
+)
+
+type status string
+
+const (
+	responseOK    status = "ok"
+	responseError status = "error"
+)
+
+// Response holds raw response from Flaresolverr API.
+type Response struct {
+	Status         status   `json:"status,omitempty"`
+	Message        string   `json:"message,omitempty"`
+	Solution       Solution `json:"solution"`
+	StartTimestamp int64    `json:"startTimestamp"`
+	EndTimestamp   int64    `json:"endTimestamp"`
+	Version        string   `json:"version"`
+}
+
+// Solution holds scraped web page from Flaresolverr API.
+type Solution struct {
+	URL       string          `json:"url"`
+	Status    int             `json:"status"`
+	Cookies   Cookies         `json:"cookies"`
+	UserAgent string          `json:"userAgent"`
+	Response  json.RawMessage `json:"response"`
+}
+
+// Cookies is a helper to manage cookie from Flaresolverr API.
+type Cookies []http.Cookie
+
+type cookie struct {
+	Name     string `json:"name,omitempty"`
+	Value    string `json:"value,omitempty"`
+	Path     string `json:"path,omitempty"`
+	Domain   string `json:"domain,omitempty"`
+	Expiry   int64  `json:"expiry,omitempty"`
+	HttpOnly bool   `json:"httpOnly,omitempty"`
+	Secure   bool   `json:"secure,omitempty"`
+	SameSite string `json:"sameSite,omitempty"`
+}
+
+// MarshalJSON is a custom function for marshalling Cookies.
+func (c Cookies) MarshalJSON() ([]byte, error) {
+	if len(c) == 0 {
+		return []byte(`""`), nil
+	}
+
+	cookies := make([]cookie, 0, len(c))
+	for _, cs := range c {
+		var sameSite string
+		switch cs.SameSite {
+		case http.SameSiteStrictMode:
+			sameSite = "Strict"
+		case http.SameSiteNoneMode:
+			sameSite = "None"
+		case http.SameSiteLaxMode:
+			sameSite = "Lax"
+		}
+		cookies = append(cookies, cookie{
+			Name:     cs.Name,
+			Value:    cs.Value,
+			Path:     cs.Path,
+			Domain:   cs.Domain,
+			Expiry:   cs.Expires.Unix(),
+			HttpOnly: cs.HttpOnly,
+			Secure:   cs.Secure,
+			SameSite: sameSite,
+		})
+	}
+
+	return json.Marshal(cookies)
+}
+
+// UnmarshalJSON is a custom function for UnmarshalJSON Cookies.
+func (c *Cookies) UnmarshalJSON(b []byte) error {
+	var cookies []cookie
+	err := json.Unmarshal(b, &cookies)
+	if err != nil {
+		return err
+	}
+
+	*c = make(Cookies, 0, len(cookies))
+	for _, cs := range cookies {
+		t := time.Unix(cs.Expiry, 0)
+		*c = append(*c, http.Cookie{
+			Name:     cs.Name,
+			Value:    cs.Value,
+			Path:     cs.Path,
+			Domain:   cs.Domain,
+			Expires:  t,
+			Secure:   cs.Secure,
+			HttpOnly: cs.HttpOnly,
+			SameSite: 0,
+		})
+	}
+
+	return nil
+}
+
+// GetParams holds parameters for calling Get.
+type GetParams struct {
+	URL     string
+	Session string
+	// MaxTimeout to solve the challenge in milliseconds for current API
+	// It replaces global timeout from Config.Timeout. Default: No timeout.
+	MaxTimeout        int
+	Cookies           Cookies
+	ReturnOnlyCookies bool
+	Proxy             string
+}
+
+type getRequest struct {
+	Cmd               command `json:"cmd"`
+	URL               string  `json:"url"`
+	Session           string  `json:"session"`
+	MaxTimeout        int     `json:"maxTimeout"`
+	Cookies           Cookies `json:"cookies"`
+	ReturnOnlyCookies bool    `json:"returnOnlyCookies"`
+	Proxy             string  `json:"proxy"`
+}
+
+// Get requests web page with method http.Get and returns Solution.Response as raw bytes.
+// For more detail, refer to https://github.com/FlareSolverr/FlareSolverr#-requestget.
+func (c *Client) Get(p GetParams) ([]byte, error) {
+	r, err := c.GetRaw(p)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Status != responseOK {
+		return nil, errors.New(r.Message)
+	}
+
+	return r.Solution.Response, nil
+}
+
+// GetRaw requests web page with method http.Get and returns whole Response.
+// For more detail, refer to https://github.com/FlareSolverr/FlareSolverr#-requestget.
+func (c *Client) GetRaw(p GetParams) (Response, error) {
+	var timeout int
+	switch {
+	case p.MaxTimeout > 0:
+		timeout = p.MaxTimeout
+	case c.timeout > 0:
+		timeout = c.timeout
+	}
+	b, err := json.Marshal(getRequest{
+		Cmd:               get,
+		URL:               p.URL,
+		Session:           p.Session,
+		MaxTimeout:        timeout,
+		Cookies:           p.Cookies,
+		ReturnOnlyCookies: p.ReturnOnlyCookies,
+		Proxy:             p.Proxy,
+	})
+	if err != nil {
+		return Response{}, err
+	}
+
+	return c.requestURL(b)
+}
+
+type PostParams struct {
+	URL               string
+	PostData          url.Values
+	Session           string
+	MaxTimeout        int
+	Cookies           Cookies
+	ReturnOnlyCookies bool
+	Proxy             string
+}
+
+type postRequest struct {
+	Cmd               command `json:"cmd"`
+	URL               string  `json:"url"`
+	PostData          string  `json:"postData"`
+	Session           string  `json:"session"`
+	MaxTimeout        int     `json:"maxTimeout"`
+	Cookies           Cookies `json:"cookies"`
+	ReturnOnlyCookies bool    `json:"returnOnlyCookies"`
+	Proxy             string  `json:"proxy"`
+}
+
+func (c *Client) Post(p PostParams) ([]byte, error) {
+	r, err := c.PostRaw(p)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Solution.Response, nil
+}
+
+func (c *Client) PostRaw(p PostParams) (Response, error) {
+	var timeout int
+	switch {
+	case p.MaxTimeout > 0:
+		timeout = p.MaxTimeout
+	case c.timeout > 0:
+		timeout = c.timeout
+	}
+	b, err := json.Marshal(postRequest{
+		Cmd:               post,
+		URL:               p.URL,
+		PostData:          p.PostData.Encode(),
+		Session:           p.Session,
+		MaxTimeout:        timeout,
+		Cookies:           p.Cookies,
+		ReturnOnlyCookies: p.ReturnOnlyCookies,
+		Proxy:             p.Proxy,
+	})
+	if err != nil {
+		return Response{}, err
+	}
+
+	return c.requestURL(b)
+}
+
+func (c *Client) requestURL(cmd []byte) (Response, error) {
+	client := new(http.Client)
+	r, err := client.Post(c.baseURL, contentApplicationJSON, bytes.NewReader(cmd))
+	if err != nil {
+		return Response{}, err
+	}
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		return Response{}, err
+	}
+
+	var resp Response
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return Response{}, err
+	}
+
+	return resp, nil
+}
---
+++ flaresolverr_test.go
@@ -0,0 +1,325 @@
+package flaresolverr
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_Get(t *testing.T) {
+	tests := []struct {
+		desc              string
+		method            command
+		url               string
+		session           string
+		maxTimeout        int
+		cookies           Cookies
+		returnOnlyCookies bool
+		proxy             string
+		handlerFunc       func(t *testing.T, p getRequest) http.HandlerFunc
+		expected          Response
+		isError           assert.ErrorAssertionFunc
+	}{
+		{
+			desc:       "GET url with no error returns response",
+			method:     get,
+			url:        "https://try.me",
+			session:    "",
+			maxTimeout: 5000,
+			cookies: Cookies{
+				{
+					Name:    "OGPC",
+					Value:   "19033459-1:",
+					Path:    "/",
+					Domain:  ".try.me",
+					Expires: time.Unix(1679759834, 0),
+				},
+			},
+			returnOnlyCookies: false,
+			proxy:             "",
+			handlerFunc: func(t *testing.T, p getRequest) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					b, err := io.ReadAll(r.Body)
+					defer r.Body.Close()
+					assert.NoError(t, err)
+
+					var test getRequest
+					err = json.Unmarshal(b, &test)
+					assert.NoError(t, err)
+					assert.Equal(t, p, test)
+
+					b, err = os.ReadFile("./testdata/response_get_success.json")
+					assert.NoError(t, err)
+
+					_, err = w.Write(b)
+					assert.NoError(t, err)
+				}
+			},
+			expected: Response{
+				Status:  responseOK,
+				Message: "Challenge not detected!",
+				Solution: Solution{
+					URL:    "https://try.me/",
+					Status: 200,
+					Cookies: Cookies{
+						http.Cookie{
+							Name:    "OGPC",
+							Value:   "19033459-1:",
+							Path:    "/",
+							Domain:  ".try.me",
+							Expires: time.Unix(1679759834, 0),
+						},
+						http.Cookie{
+							Name:    "1P_JAR",
+							Value:   "2023-01-24-15",
+							Path:    "/",
+							Domain:  ".try.me",
+							Secure:  true,
+							Expires: time.Unix(1677167834, 0),
+						},
+					},
+					UserAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+					Response:  json.RawMessage(`"<html lang=\"en\">TRY_ME</html>"`),
+				},
+				StartTimestamp: 1674575494857,
+				EndTimestamp:   1674575499113,
+				Version:        "3.0.2",
+			},
+			isError: assert.NoError,
+		},
+		{
+			desc:       "GET url with error returns error",
+			method:     get,
+			url:        "https://try.me",
+			session:    "",
+			maxTimeout: 5000,
+			cookies: Cookies{
+				{
+					Name:    "OGPC",
+					Value:   "19033459-1:",
+					Path:    "/",
+					Domain:  ".try.me",
+					Expires: time.Unix(1679759834, 0),
+				},
+			},
+			returnOnlyCookies: false,
+			proxy:             "",
+			handlerFunc: func(t *testing.T, p getRequest) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					b, err := io.ReadAll(r.Body)
+					defer r.Body.Close()
+					assert.NoError(t, err)
+
+					var test getRequest
+					err = json.Unmarshal(b, &test)
+					assert.NoError(t, err)
+					assert.Equal(t, p, test)
+
+					b, err = os.ReadFile("./testdata/response_error.json")
+					assert.NoError(t, err)
+
+					_, err = w.Write(b)
+					assert.NoError(t, err)
+				}
+			},
+			expected: Response{
+				Status:         responseError,
+				Message:        "Error: Not implemented yet.",
+				Solution:       Solution{},
+				StartTimestamp: 1674574599950,
+				EndTimestamp:   1674574599952,
+				Version:        "3.0.2",
+			},
+			isError: assert.NoError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			ts := httptest.NewServer(test.handlerFunc(t, getRequest{
+				Cmd:               test.method,
+				URL:               test.url,
+				Session:           test.session,
+				MaxTimeout:        test.maxTimeout,
+				Cookies:           test.cookies,
+				ReturnOnlyCookies: test.returnOnlyCookies,
+				Proxy:             test.proxy,
+			}))
+			defer ts.Close()
+
+			c, err := NewClient(Config{
+				BaseURL: ts.URL,
+				Timeout: 0,
+			})
+			assert.NoError(t, err)
+
+			resp, err := c.GetRaw(GetParams{
+				URL:               test.url,
+				Session:           test.session,
+				MaxTimeout:        test.maxTimeout,
+				Cookies:           test.cookies,
+				ReturnOnlyCookies: test.returnOnlyCookies,
+				Proxy:             test.proxy,
+			})
+			test.isError(t, err)
+			assert.Equal(t, test.expected, resp)
+		})
+	}
+}
+
+func TestClient_Post(t *testing.T) {
+	tests := []struct {
+		desc              string
+		method            command
+		url               string
+		postDate          url.Values
+		session           string
+		maxTimeout        int
+		cookies           Cookies
+		returnOnlyCookies bool
+		proxy             string
+		handlerFunc       func(t *testing.T, p postRequest) http.HandlerFunc
+		expected          Response
+		isError           assert.ErrorAssertionFunc
+	}{
+		{
+			desc:   "POST url with no error returns response",
+			method: post,
+			url:    "https://try.me/form-post-tester.php",
+			postDate: map[string][]string{
+				"q": {"test1"},
+				"v": {"test2"},
+			},
+			session:    "",
+			maxTimeout: 5000,
+			cookies: Cookies{
+				{
+					Name:    "OGPC",
+					Value:   "19033459-1:",
+					Path:    "/",
+					Domain:  ".try.me",
+					Expires: time.Unix(1679759834, 0),
+				},
+			},
+			returnOnlyCookies: false,
+			proxy:             "",
+			handlerFunc: func(t *testing.T, p postRequest) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					b, err := io.ReadAll(r.Body)
+					defer r.Body.Close()
+					assert.NoError(t, err)
+
+					var test postRequest
+					err = json.Unmarshal(b, &test)
+					assert.NoError(t, err)
+					assert.Equal(t, p, test)
+
+					b, err = os.ReadFile("./testdata/response_post_success.json")
+					assert.NoError(t, err)
+
+					_, err = w.Write(b)
+					assert.NoError(t, err)
+				}
+			},
+			expected: Response{
+				Status:  responseOK,
+				Message: "Challenge not detected!",
+				Solution: Solution{
+					URL:       "https://try.me/form-post-tester.php",
+					Status:    200,
+					Cookies:   Cookies{},
+					UserAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+					Response:  json.RawMessage(`"<html><head><meta name=\"color-scheme\" content=\"light dark\"></head><body><pre style=\"word-wrap: break-word; white-space: pre-wrap;\">The POSTed value is:\n********************\nkey1=valueA&amp;key2=valueB\n********************\n\n\nThe Headers are:\n********************\nHost: www.hashemian.com\nConnection: Keep-Alive\nAccept-Encoding: gzip\nX-Forwarded-For: 45.77.173.90\nCF-RAY: 78ea349188884cdd-EWR\nContent-Length: 23\nX-Forwarded-Proto: https\nCF-Visitor: {\"scheme\":\"https\"}\ncache-control: max-age=0\nsec-ch-ua: \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\"\nsec-ch-ua-mobile: ?0\nsec-ch-ua-platform: \"Linux\"\nupgrade-insecure-requests: 1\norigin: null\ncontent-type: application/x-www-form-urlencoded\nuser-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36\naccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\nsec-fetch-site: cross-site\nsec-fetch-mode: navigate\nsec-fetch-dest: document\naccept-language: en-US,en;q=0.9\nCF-Connecting-IP: 45.77.173.90\nCF-IPCountry: SG\nCDN-Loop: cloudflare\n********************\n\n</pre></body></html>"`),
+				},
+				StartTimestamp: 1674578364632,
+				EndTimestamp:   1674578368933,
+				Version:        "3.0.2",
+			},
+			isError: assert.NoError,
+		},
+		{
+			desc:       "POST url with error returns error",
+			method:     post,
+			url:        "https://try.me/form-post-tester.php",
+			session:    "",
+			maxTimeout: 5000,
+			cookies: Cookies{
+				{
+					Name:    "OGPC",
+					Value:   "19033459-1:",
+					Path:    "/",
+					Domain:  ".try.me",
+					Expires: time.Unix(1679759834, 0),
+				},
+			},
+			returnOnlyCookies: false,
+			proxy:             "",
+			handlerFunc: func(t *testing.T, p postRequest) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					b, err := io.ReadAll(r.Body)
+					defer r.Body.Close()
+					assert.NoError(t, err)
+
+					var test postRequest
+					err = json.Unmarshal(b, &test)
+					assert.NoError(t, err)
+					assert.Equal(t, p, test)
+
+					b, err = os.ReadFile("./testdata/response_error.json")
+					assert.NoError(t, err)
+
+					_, err = w.Write(b)
+					assert.NoError(t, err)
+				}
+			},
+			expected: Response{
+				Status:         responseError,
+				Message:        "Error: Not implemented yet.",
+				Solution:       Solution{},
+				StartTimestamp: 1674574599950,
+				EndTimestamp:   1674574599952,
+				Version:        "3.0.2",
+			},
+			isError: assert.NoError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			ts := httptest.NewServer(test.handlerFunc(t, postRequest{
+				Cmd:               test.method,
+				URL:               test.url,
+				Session:           test.session,
+				MaxTimeout:        test.maxTimeout,
+				Cookies:           test.cookies,
+				ReturnOnlyCookies: test.returnOnlyCookies,
+				Proxy:             test.proxy,
+			}))
+			defer ts.Close()
+
+			c, err := NewClient(Config{
+				BaseURL: ts.URL,
+				Timeout: 0,
+			})
+			assert.NoError(t, err)
+
+			resp, err := c.PostRaw(PostParams{
+				URL:               test.url,
+				Session:           test.session,
+				MaxTimeout:        test.maxTimeout,
+				Cookies:           test.cookies,
+				ReturnOnlyCookies: test.returnOnlyCookies,
+				Proxy:             test.proxy,
+			})
+			test.isError(t, err)
+			assert.Equal(t, test.expected, resp)
+		})
+	}
+}
